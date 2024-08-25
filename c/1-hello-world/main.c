d51dfc5f595224058f6da4b0ac49ea08c1e29640
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2024-08-25 19:13:06 +0200

    c: static link libgpio

--- c/1-hello-world/main.c
+++ c/1-hello-world/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <gpiod.h>
+#include <gpiod.h>
 
 int main(int, char *[]) {
-  printf("hello world");
+  printf("hello world\n");
   return EXIT_SUCCESS;
 }
