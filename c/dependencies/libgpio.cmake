d6c39375e8b7ee5788ddc74c8caa7e5235ce4fd5
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2024-08-25 21:14:13 +0200

    FetchContent -> ExternalProject

--- c/dependencies/libgpio.cmake
+++ c/dependencies/libgpio.cmake
@@ -1,44 +1,31 @@
 # Provides `libgpiod` library target
 
-include(FetchContent)
+include(ExternalProject)
 
-# ===== FETCH =================================================================
-FetchContent_Declare(
+set(LIBGPIOD_PREFIX "${CMAKE_BINARY_DIR}/_deps/libgpiod")
+set(LIBGPIOD_LIBRARY "${LIBGPIOD_PREFIX}/lib/libgpiod.a")
+set(LIBGPIOD_INCLUDE_DIR "${LIBGPIOD_PREFIX}/include")
+set(LIBGPIOD_HEADER "${LIBGPIOD_INCLUDE_DIR}/gpiod.h")
+
+# ===== FETCH, CONFIGURE, BUILD, INSTALL ======================================
+ExternalProject_Add(
   libgpiod_git
+  PREFIX "${LIBGPIOD_PREFIX}"
   GIT_REPOSITORY "https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git"
   GIT_TAG v2.0.1
-  UPDATE_DISCONNECTED ON)
-FetchContent_MakeAvailable(libgpiod_git)
-
-# ===== BUILD =================================================================
-set(libgpiod_git_INSTALL_DIR ${libgpiod_git_BINARY_DIR}/install)
-
-add_custom_command(
-  OUTPUT ${libgpiod_git_BINARY_DIR}/Makefile
-  COMMAND ${libgpiod_git_SOURCE_DIR}/autogen.sh --host=${CMAKE_SYSTEM_PROCESSOR}
-          --prefix=${libgpiod_git_INSTALL_DIR}
-  WORKING_DIRECTORY ${libgpiod_git_BINARY_DIR})
-
-add_custom_target(
-  libgpiod_make
-  COMMAND make
-  DEPENDS ${libgpiod_git_BINARY_DIR}/Makefile
-  WORKING_DIRECTORY ${libgpiod_git_BINARY_DIR})
-
-add_custom_command(
-  OUTPUT ${libgpiod_git_INSTALL_DIR}/lib/libgpiod.a
-  COMMAND make install
-  DEPENDS libgpiod_make
-  WORKING_DIRECTORY ${libgpiod_git_BINARY_DIR})
-
-add_custom_target(libgpiod_bin
-                  DEPENDS ${libgpiod_git_INSTALL_DIR}/lib/libgpiod.a)
+  UPDATE_DISCONNECTED ON
+  CONFIGURE_COMMAND <SOURCE_DIR>/autogen.sh --host=${CMAKE_SYSTEM_PROCESSOR}
+                    --prefix=<INSTALL_DIR>
+  BUILD_COMMAND make
+  INSTALL_COMMAND make install INSTALL_BYPRODUCTS "${LIBGPIOD_LIBRARY}"
+                  "${LIBGPIOD_HEADER}")
 
 # ===== MAKE TARGET ===========================================================
+# hack needed to avoid errors about non-existing directory
+file(MAKE_DIRECTORY "${LIBGPIOD_INCLUDE_DIR}")
+
 add_library(libgpiod SHARED IMPORTED)
-add_dependencies(libgpiod libgpiod_bin)
+add_dependencies(libgpiod libgpiod_git)
 set_target_properties(
-  libgpiod PROPERTIES IMPORTED_LOCATION
-                      ${libgpiod_git_INSTALL_DIR}/lib/libgpiod.a)
-target_include_directories(libgpiod
-                           INTERFACE ${libgpiod_git_SOURCE_DIR}/include)
+  libgpiod PROPERTIES IMPORTED_LOCATION "${LIBGPIOD_LIBRARY}"
+                      INTERFACE_INCLUDE_DIRECTORIES "${LIBGPIOD_INCLUDE_DIR}")
