3d19f634b77864b7639d7d20621311a2ecfa4a6a
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-01-12 22:10:05 +0100

    rust: convert to hardware pwm

--- rust/2-motor-controller/src/main.rs
+++ rust/2-motor-controller/src/main.rs
@@ -5,9 +5,9 @@ use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
-use rppal::gpio::Gpio;
+use rppal::pwm::{Channel, Pwm};
 
-const GPIO_MOTOR: u8 = 13;
+const PWM_CHANNEL: Channel = Channel::Pwm1; // GPIO 13
 const PERIOD_MS: u64 = 5000;
 const PWM_CHANGES: u64 = 50;
 const PWM_MIN: f64 = 0.2;
@@ -18,8 +18,8 @@ const SLEEP_DURATION: Duration = Duration::from_millis(PERIOD_MS / PWM_CHANGES);
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Controlling motor from Rust.");
 
-    let gpio = Gpio::new()?;
-    let mut pin = gpio.get(GPIO_MOTOR)?.into_output();
+    let pwm = Pwm::new(PWM_CHANNEL)?;
+    pwm.enable()?;
 
     let more_work = Arc::new(AtomicBool::new(true));
     {
@@ -35,8 +35,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             let sin = f64::sin(i as f64 / PWM_CHANGES as f64 * 2. * PI);
             let ratio = (sin + 1.) / 2.;
             let duty_cycle = PWM_MIN + ratio * (PWM_MAX - PWM_MIN);
-            _ = pin
-                .set_pwm_frequency(PWM_FREQUENCY, duty_cycle)
+            _ = pwm
+                .set_frequency(PWM_FREQUENCY, duty_cycle)
                 .inspect_err(|err| eprintln!("error setting pwm frequency: {err}"));
 
             if !more_work.load(Ordering::Relaxed) {
@@ -46,6 +46,5 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    pin.clear_pwm()?;
     Ok(())
 }
