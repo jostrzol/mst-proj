60e2da1bf7c817e3f4984e8271f906d80e43c71e
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-02-05 19:27:33 +0100

    rust: remove sine wave residues

--- rust/2-motor-controller/src/main.rs
+++ rust/2-motor-controller/src/main.rs
@@ -1,5 +1,6 @@
+#![feature(duration_constants)]
+
 use std::error::Error;
-use std::f64::consts::PI;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::thread;
@@ -9,12 +10,10 @@ use rppal::i2c::I2c;
 use rppal::pwm::{Channel, Pwm};
 
 const PWM_CHANNEL: Channel = Channel::Pwm1; // GPIO 13
-const PERIOD_MS: u64 = 5000;
-const PWM_CHANGES: u64 = 50;
-const PWM_MIN: f64 = 0.2;
-const PWM_MAX: f64 = 1.0;
 const PWM_FREQUENCY: f64 = 1000.;
-const SLEEP_DURATION: Duration = Duration::from_millis(PERIOD_MS / PWM_CHANGES);
+const REFRESH_RATE: u64 = 60;
+const SLEEP_DURATION: Duration =
+    Duration::from_millis(Duration::SECOND.as_millis() as u64 / REFRESH_RATE);
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Controlling motor from Rust.");
@@ -35,38 +34,30 @@ fn main() -> Result<(), Box<dyn Error>> {
         })?;
     }
 
-    'outer: loop {
-        for i in 0..PWM_CHANGES {
-            let value = match read_potentiometer_value(&mut i2c) {
-                Ok(value) => value,
-                Err(err) => {
-                    eprintln!("error reading potentiometer value: {err}");
-                    continue;
-                }
-            };
-            println!("potentiometer: {value}");
+    while more_work.load(Ordering::Relaxed) {
+        thread::sleep(SLEEP_DURATION);
 
-            // let sin = f64::sin(i as f64 / PWM_CHANGES as f64 * 2. * PI);
-            // let ratio = (sin + 1.) / 2.;
-            // let duty_cycle = PWM_MIN + ratio * (PWM_MAX - PWM_MIN);
-            let duty_cycle = value as f64 / u8::MAX as f64;
-            _ = pwm
-                .set_frequency(PWM_FREQUENCY, duty_cycle)
-                .inspect_err(|err| eprintln!("error setting pwm frequency: {err}"));
+        let Some(value) = read_potentiometer_value(&mut i2c) else {
+            continue;
+        };
 
-            if !more_work.load(Ordering::Relaxed) {
-                break 'outer;
-            }
-            thread::sleep(SLEEP_DURATION);
+        let duty_cycle = value as f64 / u8::MAX as f64;
+        println!("selected duty cycle: {duty_cycle:.2}");
+
+        if let Err(err) = pwm.set_frequency(PWM_FREQUENCY, duty_cycle) {
+            eprintln!("error setting pwm frequency: {err}");
         }
     }
 
     Ok(())
 }
 
-fn read_potentiometer_value(i2c: &mut I2c) -> Result<u8, Box<dyn Error>> {
+fn read_potentiometer_value(i2c: &mut I2c) -> Option<u8> {
     const WRITE_BUFFER: [u8; 1] = [0x84];
-    let mut read_buffer = [0; 1];
-    i2c.write_read(&WRITE_BUFFER, &mut read_buffer)?;
-    Ok(read_buffer[0])
+    let mut read_buffer = [0];
+
+    i2c.write_read(&WRITE_BUFFER, &mut read_buffer)
+        .inspect_err(|err| eprintln!("error reading potentiometer value: {err}"))
+        .ok()?;
+    Some(read_buffer[0])
 }
