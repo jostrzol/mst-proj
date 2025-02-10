194c1e00ce3da88101837d755bac0fe7c1035934
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-02-10 23:49:19 +0100

    zig: complete

--- zig/2-motor-controller/main.zig
+++ zig/2-motor-controller/main.zig
@@ -13,12 +13,9 @@ const i2c_adapter_path = std.fmt.comptimePrint("/dev/i2c-{}", .{i2c_adapter_numb
 const ads7830_address: c_int = 0x48;
 const motor_pwm_channel: u8 = 1; // gpio 13
 
-const period_ms: u64 = 5000;
-const pwm_updates_per_period: u64 = 50;
-const pwm_min: f32 = 0.2;
-const pwm_max: f32 = 1.0;
 const pwm_frequency: f32 = 1000;
-const sleep_time_ns: u64 = period_ms * std.time.ns_per_ms / pwm_updates_per_period;
+const refresh_rate: u64 = 60;
+const sleep_time_ns: u64 = std.time.ns_per_ms / refresh_rate;
 
 var do_continue = true;
 pub fn interrupt_handler(_: c_int) callconv(.C) void {
@@ -28,6 +25,34 @@ pub fn interrupt_handler(_: c_int) callconv(.C) void {
 const interrupt_sigaction = c.struct_sigaction{
     .__sigaction_handler = .{ .sa_handler = &interrupt_handler },
 };
+
+fn make_read_command(comptime channel: u8) u8 {
+    comptime std.debug.assert(channel < 7);
+
+    // bit    7: single-ended inputs mode
+    // bits 6-4: channel selection
+    // bit    3: is internal reference enabled
+    // bit    2: is converter enabled
+    // bits 1-0: unused
+    const default_read_command = 0b10001100;
+
+    return default_read_command & (channel << 4);
+}
+
+fn read_potentiometer_value(i2c_file: std.fs.File) ?u8 {
+    if (c.i2c_smbus_write_byte(i2c_file.handle, make_read_command(0)) < 0) {
+        std.log.err("writing i2c ADC command failed\n", .{});
+        return null;
+    }
+
+    const value = c.i2c_smbus_read_byte(i2c_file.handle);
+    if (value < 0) {
+        std.log.err("reading i2c ADC value failed\n", .{});
+        return null;
+    }
+
+    return @intCast(value);
+}
 
 pub fn main() !void {
     if (c.sigaction(c.SIGINT, &interrupt_sigaction, null) != 0) {
@@ -58,28 +83,18 @@ pub fn main() !void {
     });
     try channel.enable();
 
-    main_loop: while (true) {
-        for (0..pwm_updates_per_period) |i| {
-            _ = c.i2c_smbus_write_byte(i2c_file.handle, 0x84);
-            const value = c.i2c_smbus_read_byte(i2c_file.handle);
-            std.debug.print("selected duty cycle: {}", .{value});
+    while (do_continue) {
+        std.time.sleep(sleep_time_ns);
 
-            updateDutyCycle(channel, i) catch |err| {
-                std.debug.print("error updating duty cycle: {}", .{err});
-            };
+        const value = read_potentiometer_value(i2c_file) orelse continue;
+        const duty_cycle = @as(f32, @floatFromInt(value)) / std.math.maxInt(u8);
+        std.debug.print("selected duty cycle: {d:.2}\n", .{duty_cycle});
 
-            if (!do_continue) break :main_loop;
-            std.time.sleep(sleep_time_ns);
-        }
+        channel.setParameters(.{
+            .frequency = null,
+            .duty_cycle_ratio = duty_cycle,
+        }) catch |err| {
+            std.debug.print("error updating duty cycle: {}\n", .{err});
+        };
     }
-}
-fn updateDutyCycle(channel: *pwm.Channel, step: u64) !void {
-    const sin_arg_ratio = @as(f32, @floatFromInt(step)) / pwm_updates_per_period;
-    const sin = std.math.sin(sin_arg_ratio * 2 * std.math.pi);
-    const ratio = (sin + 1) / 2;
-    const duty_cycle = pwm_min + ratio * (pwm_max - pwm_min);
-    try channel.setParameters(.{
-        .frequency = null,
-        .duty_cycle_ratio = duty_cycle,
-    });
 }
