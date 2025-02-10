d4a982641dd6d6baec0d95f98c64f52779b80c96
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-02-10 23:28:33 +0100

    zig: remove unnecesary funcs from build.zig

--- zig/2-motor-controller/build.zig
+++ zig/2-motor-controller/build.zig
@@ -1,12 +1,4 @@
 const std = @import("std");
-const Sha256 = std.crypto.hash.sha2.Sha256;
-
-const rpiTargetQuery = std.Target.Query{
-    .cpu_arch = .arm,
-    .cpu_model = .{ .explicit = &std.Target.arm.cpu.arm1176jzf_s },
-    .os_tag = .linux,
-    .abi = .gnueabihf,
-};
 
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
@@ -43,40 +35,4 @@ pub fn build(b: *std.Build) void {
 
     const check_step = b.step("check", "Check the application");
     check_step.dependOn(&exe.step);
-}
-
-fn makeCachedTempDir(b: *std.Build, dependency: []const u8, dir_key: []const u8) !std.Build.LazyPath {
-    const pkg_hash = findPkgHashOrFatal(b, dependency);
-
-    const full_key = try std.mem.concat(b.allocator, u8, &[_][]const u8{ pkg_hash, dir_key });
-    defer b.allocator.free(full_key);
-
-    var full_hash: [Sha256.digest_length]u8 = undefined;
-    Sha256.hash(full_key, &full_hash, .{});
-
-    const full_hash_hex = std.Build.hex64(@bitCast(full_hash[0..8].*));
-
-    const result = try b.cache_root.join(b.allocator, &[_][]const u8{ "tmp", &full_hash_hex });
-    return std.Build.LazyPath{ .cwd_relative = result };
-}
-
-// Copied from Build.zig
-fn findPkgHashOrFatal(b: *std.Build, name: []const u8) []const u8 {
-    for (b.available_deps) |dep| {
-        if (std.mem.eql(u8, dep[0], name)) return dep[1];
-    }
-
-    const full_path = b.pathFromRoot("build.zig.zon");
-    std.debug.panic("no dependency named '{s}' in '{s}'. All packages used in build.zig must be declared in this file", .{ name, full_path });
-}
-
-fn formatTarget(
-    allocator: std.mem.Allocator,
-    target: *const std.Build.ResolvedTarget,
-) std.fmt.AllocPrintError![]const u8 {
-    return std.fmt.allocPrint(allocator, "{s}-{s}-{s}", .{
-        target.result.osArchName(),
-        @tagName(target.result.os.tag),
-        @tagName(target.result.abi),
-    });
 }
