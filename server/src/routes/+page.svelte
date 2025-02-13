dafcc00e2938e6b5f135e4d9301df6716a19da83
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-02-13 18:36:59 +0100

    server: add charts

--- server/src/routes/+page.svelte
+++ server/src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1 class="">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { LineChart } from 'layerchart';
+	import { ticks } from 'd3-array';
+
+	let dynamicData = ticks(-2, 2, 200).map(Math.sin);
+
+	let renderContext: 'svg' | 'canvas' = 'svg';
+	let debug = false;
+</script>
+
+<h2>Dynamic data (move over chart)</h2>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+	class="h-[300px] rounded border p-4"
+	on:mousemove={(e) => {
+		const x = e.clientX;
+		const y = e.clientY;
+		dynamicData = dynamicData.slice(-200).concat(Math.atan2(x, y));
+	}}
+>
+	<LineChart
+		data={dynamicData.map((d, i) => ({ x: i, y: d }))}
+		x="x"
+		y="y"
+		yBaseline={undefined}
+		tooltip={{ mode: 'manual' }}
+		props={{ yAxis: { tweened: true }, grid: { tweened: true } }}
+		{renderContext}
+		{debug}
+	/>
+</div>
+
+<h2>Brushing</h2>
+
+<div class="h-[300px] rounded border p-4">
+	<LineChart
+		data={dynamicData}
+		x="date"
+		y="value"
+		brush
+		props={{
+			spline: { tweened: { duration: 200 } },
+			xAxis: { format: undefined, tweened: { duration: 200 } }
+		}}
+		{renderContext}
+		{debug}
+	/>
+</div>
