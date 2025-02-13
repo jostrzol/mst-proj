03d924ff8c2d595540fa89fe940a9710dfced2f0
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-02-13 17:40:44 +0100

    server: reinit

--- server/src/routes/+layout.svelte
+++ server/src/routes/+layout.svelte
@@ -1,26 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-
-	import { AppBar, AppLayout, ThemeInit, ThemeSwitch } from 'svelte-ux';
-
 	let { children } = $props();
 </script>
 
-<ThemeInit />
-
-<AppLayout areas="'header header' 'aside main'" navWidth={0}>
-	<!-- <svelte:fragment slot="nav"> -->
-	<!-- 	<p>1</p> -->
-	<!-- 	<p>2</p> -->
-	<!-- </svelte:fragment> -->
-
-	<AppBar title="Motor speed regulator" menuIcon={null}>
-		<div slot="actions">
-			<ThemeSwitch />
-		</div>
-	</AppBar>
-
-	<main class="p-4 ">
-		{@render children()}
-	</main>
-</AppLayout>
+{@render children()}
