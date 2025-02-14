629ccadaa00393df542b82fda6eadfcef023cf6f
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-02-14 15:56:30 +0100

    server: add control series

--- server/src/routes/+page.svelte
+++ server/src/routes/+page.svelte
@@ -14,9 +14,10 @@
 
 	const tMsStart = Date.now();
 
-	let xsRaw = $state(new RingBuffer(Uint32Array, PTS_WINDOW));
-	let y1sRaw = $state(new RingBuffer(Uint8Array, PTS_WINDOW));
-	let y2sRaw = $state(new RingBuffer(Uint8Array, PTS_WINDOW));
+	let xsRaw = new RingBuffer(Uint32Array, PTS_WINDOW);
+	let y1sRaw = new RingBuffer(Uint8Array, PTS_WINDOW);
+	let y2sRaw = new RingBuffer(Uint8Array, PTS_WINDOW);
+	let y3sRaw = new RingBuffer(Uint8Array, PTS_WINDOW);
 
 	let sample = $state(true);
 
@@ -29,9 +30,11 @@
 					.map((i) => now - tMsStart - i * SAMPLE_INTERVAL_MS);
 				const ys1ToAdd = xsToAdd.map((tMs) => ((Math.sin(tMs / 1000) + 1) / 2) * 255);
 				const ys2ToAdd = xsToAdd.map((tMs) => ((Math.cos(tMs / 1000) + 1) / 2) * 255);
+				const ys3ToAdd = xsToAdd.map((tMs) => ((Math.sin(tMs / 1000 + 1) + 1) / 2) * 255);
 
 				y1sRaw.push(...ys1ToAdd);
 				y2sRaw.push(...ys2ToAdd);
+				y3sRaw.push(...ys3ToAdd);
 				xsRaw.push(...xsToAdd);
 			}
 		}, SAMPLE_INTERVAL_MS * SAMPLE_BATCH_SIZE);
@@ -41,6 +44,7 @@
 	let xs = $state.raw(new Array<number>());
 	let y1s = $state.raw(new Array<number>());
 	let y2s = $state.raw(new Array<number>());
+	let y3s = $state.raw(new Array<number>());
 
 	let tMsDraw = $state(0);
 	const tSDraw = $derived(tMsDraw / 1000);
@@ -50,9 +54,10 @@
 	$effect(() => {
 		let frame = requestAnimationFrame(function loop() {
 			if (draw) {
-				xs = untrack(() => [...xsRaw]);
-				y1s = untrack(() => [...y1sRaw]);
-				y2s = untrack(() => [...y2sRaw]);
+				xs = [...xsRaw];
+				y1s = [...y1sRaw];
+				y2s = [...y2sRaw];
+				y3s = [...y3sRaw];
 			}
 			tMsDraw = Date.now() - tMsStart;
 			frame = requestAnimationFrame(loop);
@@ -65,11 +70,12 @@
 	const rawSeries = $derived([
 		{ name: 'Read frequency [Hz]', rawData: y1s, color: 'red' },
 		{ name: 'Set frequency [Hz]', rawData: y2s, color: 'green' },
+		{ name: 'Control signal', rawData: y3s, color: 'yellow' },
 	]);
 	const series = $derived(
 		rawSeries.map((series) => ({
 			...series,
-			data: [...series.rawData].map((value, i) => ({
+			data: series.rawData.map((value, i) => ({
 				x: xs.at(i)! / 1000,
 				y: value,
 				c: series.name,
@@ -77,7 +83,8 @@
 		})),
 	);
 
-	const combinedData = $derived(series.flatMap((series) => series.data));
+	const frequencySeries = $derived(series.slice(0, 2));
+	const controlSeries = $derived(series.slice(2, 3));
 </script>
 
 <div class="m-4">
@@ -93,10 +100,16 @@
 		</label>
 	</div>
 
+	{@render chart(frequencySeries)}
+
+	{@render chart(controlSeries)}
+</div>
+
+{#snippet chart(localSeries: typeof series)}
 	<div class="h-[300px] rounded border p-4" role="img">
 		<div class="h-full w-full overflow-hidden">
 			<Chart
-				data={combinedData}
+				data={localSeries.flatMap(series => series.data)}
 				x="x"
 				{xDomain}
 				y="y"
@@ -104,20 +117,21 @@
 				yNice
 				c="c"
 				cScale={scaleOrdinal()}
-				cDomain={series.map(({ name }) => name)}
-				cRange={series.map(({ color }) => color)}
+				cDomain={localSeries.map(({ name }) => name)}
+				cRange={localSeries.map(({ color }) => color)}
 				padding={{ left: 16, bottom: 48, right: 16 }}
 				tooltip={{ mode: 'voronoi' }}
 			>
 				<Canvas>
 					<Axis placement="left" grid rule />
 					<Axis placement="bottom" rule />
-					{#each series as { data, color }}
+					{#each localSeries as { data, color }}
 						<Spline {data} class="stroke-2" stroke={color}></Spline>
 					{/each}
 				</Canvas>
+
 				<Legend placement="bottom" variant="swatches" />
 			</Chart>
 		</div>
 	</div>
-</div>
+{/snippet}
