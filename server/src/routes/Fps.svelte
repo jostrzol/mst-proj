8b26858bf2576f5275edd00303115acdc531237e
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-02-14 14:14:23 +0100

    server: fix Fps

--- server/src/routes/Fps.svelte
+++ server/src/routes/Fps.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import { scaleLinear } from 'd3-scale';
+	import { scaleLinear, scaleSequential } from 'd3-scale';
+	import { interpolateRgb, interpolateRgbBasis } from 'd3-interpolate';
 	import { untrack } from 'svelte';
 
 	const SAMPLING_INTERVAL_MS = 1000;
@@ -26,18 +27,20 @@
 
 	function hsl(color: string | undefined) {
 		if (!color) return undefined;
-		return `hsl(${color})`;
+		return `hsl(${color.split(' ').join(',')})`;
 	}
 
 	let p = $state<HTMLElement | undefined>();
-	let color = $state(scaleLinear([0, 30, 60], ['red', 'yellow', 'green']).clamp(true));
+	let colorInterpolator = $state(interpolateRgbBasis(['red', 'yellow', 'green']));
+	let color = $derived(scaleSequential([0, 30, 60], colorInterpolator).clamp(true));
 	$effect(() => {
 		const element = untrack(() => p);
 		const style = getComputedStyle(element!);
 		const colorError = hsl(style.getPropertyValue('--color-danger')) || 'red';
 		const colorWarning = hsl(style.getPropertyValue('--color-warning')) || 'yellow';
 		const colorSuccess = hsl(style.getPropertyValue('--color-success')) || 'green';
-		color = scaleLinear([0, 30, 60], [colorError, colorWarning, colorSuccess]).clamp(true);
+		const colors = [colorError, colorWarning, colorSuccess];
+		colorInterpolator = interpolateRgbBasis(colors);
 	});
 
 	const fps = $derived(((frameIndex - lastFrameIndex) / SAMPLING_INTERVAL_MS) * 1000);
