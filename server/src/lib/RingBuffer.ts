79f22e7dcec33ba8f71a99787d4f742698870f53
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-02-14 09:32:51 +0100

    server: add RingBuffer

---
+++ server/src/lib/RingBuffer.ts
@@ -0,0 +1,59 @@
+import { createSubscriber } from "svelte/reactivity";
+
+export type TypedNumberArray =
+  | Uint8Array
+  | Uint8ClampedArray
+  | Int16Array
+  | Uint16Array
+  | Int32Array
+  | Uint32Array
+  | Float32Array
+  | Float64Array
+  | BigInt64Array
+  | BigUint64Array
+  ;
+type NumberType<T extends TypedNumberArray> = Exclude<ReturnType<T["at"]>, undefined>;
+
+export class RingBuffer<T extends TypedNumberArray> {
+  #buffer: T;
+  #head_i: number = 0;
+  #length: number = 0;
+  #update: (() => void) | null = null
+  #subscribe: (() => void) | null = null
+
+  constructor(Class: new (size: number) => T, size: number) {
+    this.#buffer = new Class(size)
+    this.#subscribe = createSubscriber(update => {
+      this.#update = update
+      return () => this.#update = this.#subscribe = null
+    })
+  }
+
+  push(item: number) {
+    this.#buffer[this.#head_i] = item;
+    this.#head_i = (this.#head_i + 1) % this.#buffer.length
+    this.#length = Math.min(this.#buffer.length, this.#length + 1)
+
+    this.#update?.call(this)
+  }
+
+  get length() {
+    return this.#length
+  }
+
+  *[Symbol.iterator]() {
+    this.#subscribe?.call(this);
+
+    yield* Array(this.#length).keys().map(i => this.#buffer[this.real_index(i)]);
+  }
+
+  at(index: number): NumberType<T> | undefined {
+    this.#subscribe?.call(this);
+
+    return index > this.#length ? undefined : this.#buffer[this.real_index(index)] as any;
+  }
+
+  private real_index(index: number) {
+    return (this.#head_i + index) % this.#length;
+  }
+}
--- server/src/routes/+page.svelte
+++ server/src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import { map } from 'd3-array';
 	import { LineChart } from 'layerchart';
-	import { onMount } from 'svelte';
 	import { Checkbox } from 'svelte-ux';
+	import { RingBuffer } from '$lib';
 
 	const REFRESH_RATE = 60;
 	const INTERVAL_MS = 1000 / REFRESH_RATE;
@@ -14,15 +13,15 @@
 	let t_ms = $state(0);
 	const t_s = $derived(t_ms / 1000);
 
-	let xs = $state.raw(new Uint32Array(PTS_WINDOW));
-	let ys = $state.raw(new Uint8Array(PTS_WINDOW));
+	let xs = $state(new RingBuffer(Uint32Array, PTS_WINDOW));
+	let ys = $state(new RingBuffer(Uint8Array, PTS_WINDOW));
 
 	$effect(() => {
 		const interval = setInterval(() => {
 			if (draw) {
 				const y = ((Math.sin(t_s) + 1) / 2) * 255;
-				ys = new Uint8Array([...ys.slice(-PTS_WINDOW - 1), y]);
-				xs = new Uint32Array([...xs.slice(-PTS_WINDOW - 1), t_ms]);
+				ys.push(y);
+				xs.push(t_ms);
 			}
 			t_ms += INTERVAL_MS;
 		}, INTERVAL_MS);
@@ -31,58 +30,7 @@
 
 	const xDomain = $derived([t_s - T_S_WINDOW, t_s]);
 
-	// const plotData = $derived.by(() => {
-	// 	let last: Point = { x: Infinity, y: Infinity };
-	// 	let result: Point[] = [];
-
-	// 	for (let i = data.length - 1; i >= 0; i--) {
-	// 		const point = data[i];
-	// 		const { x } = point;
-
-	// 		if (x > xDomain[1]) continue;
-	// 		if (Math.abs(last.x - point.x) < T_S_RESOLUTION) continue;
-	// 		if (x < xDomain[0]) break;
-
-	// 		result.push(point);
-	// 		last = point;
-	// 	}
-
-	// 	return result;
-	// });
-	// let plotData: Point[] = $state.raw([]);
-	// onMount(() => {
-	// 	let frame: number;
-
-	// 	const loop = () => {
-	// 		let last: Point = { x: Infinity, y: Infinity };
-	// 		let result: Point[] = [];
-
-	// 		for (let i = data.length - 1; i >= 0; i--) {
-	// 			const point = data[i];
-	// 			const { x } = point;
-
-	// 			if (x > xDomain[1]) continue;
-
-	// 			const dT = Math.abs(last.x - point.x);
-	// 			const isLast = x < xDomain[0];
-	// 			if (dT < T_S_RESOLUTION && !isLast) continue;
-
-	// 			result.push(point);
-	// 			last = point;
-
-	// 			if (isLast) break;
-	// 		}
-
-	// 		frame = requestAnimationFrame(loop);
-	// 		plotData = result;
-	// 	};
-
-	// 	frame = requestAnimationFrame(loop);
-
-	// 	return () => cancelAnimationFrame(frame);
-	// });
-
-	const plotData = $derived([...ys].map((value, i) => ({ x: xs[i] / 1000, y: value })));
+	const plotData = $derived([...ys].map((value, i) => ({ x: xs.at(i)! / 1000, y: value })));
 </script>
 
 <h2>Dynamic data (move over chart)</h2>
