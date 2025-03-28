aa8be4d42c8e689eed102670feb6c3a935f1b3f5
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-03-28 21:52:21 +0100

    rust: blinky-bm esp-idf done

--- rust/1-blinky-bm/src/main.rs
+++ rust/1-blinky-bm/src/main.rs
@@ -1,10 +1,25 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::gpio::*;
+use esp_idf_hal::peripherals::Peripherals;
 
-    // Bind the log crate to the ESP Logging facilities
+const PERIOD_MS: u32 = 1000;
+const SLEEP_DURATION_MS: u32 = PERIOD_MS / 2;
+
+fn main() -> anyhow::Result<()> {
+    esp_idf_hal::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    log::info!("Controlling an LED from Rust");
+
+    let peripherals = Peripherals::take()?;
+    let mut led = PinDriver::output(peripherals.pins.gpio5)?;
+
+    loop {
+        log::info!(
+            "Turning the LED {}",
+            if led.is_set_low() { "ON" } else { "OFF" }
+        );
+        led.toggle()?;
+        FreeRtos::delay_ms(SLEEP_DURATION_MS);
+    }
 }
