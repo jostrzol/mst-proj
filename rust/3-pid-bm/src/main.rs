c0298e9aa4c849db8ad537d5dfde64ac8cc4c7e6
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-04-08 23:15:46 +0200

    rust: pid-bm refactor services.rs

--- rust/3-pid-bm/src/main.rs
+++ rust/3-pid-bm/src/main.rs
@@ -1,13 +1,10 @@
-use core::convert::TryInto;
-
-use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
+mod services;
 
 use esp_idf_svc::hal::prelude::Peripherals;
 use esp_idf_svc::log::EspLogger;
-use esp_idf_svc::wifi::{BlockingWifi, EspWifi};
-use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
 
 use log::info;
+use services::Services;
 
 const SSID: &str = env!("WIFI_SSID");
 const PASSWORD: &str = env!("WIFI_PASS");
@@ -17,47 +14,11 @@ fn main() -> anyhow::Result<()> {
     EspLogger::initialize_default();
 
     let peripherals = Peripherals::take()?;
-    let sys_loop = EspSystemEventLoop::take()?;
-    let nvs = EspDefaultNvsPartition::take()?;
-
-    let mut wifi = BlockingWifi::wrap(
-        EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?,
-        sys_loop,
-    )?;
-
-    connect_wifi(&mut wifi)?;
-
-    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
-
-    info!("Wifi DHCP info: {:?}", ip_info);
+    let _ = Services::new(peripherals.modem, SSID, PASSWORD)?;
 
     info!("Shutting down in 5s...");
 
     std::thread::sleep(core::time::Duration::from_secs(5));
-
-    Ok(())
-}
-
-fn connect_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> anyhow::Result<()> {
-    let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
-        ssid: SSID.try_into().unwrap(),
-        bssid: None,
-        auth_method: AuthMethod::WPA2Personal,
-        password: PASSWORD.try_into().unwrap(),
-        channel: None,
-        ..Default::default()
-    });
-
-    wifi.set_configuration(&wifi_configuration)?;
-
-    wifi.start()?;
-    info!("Wifi started");
-
-    wifi.connect()?;
-    info!("Wifi connected");
-
-    wifi.wait_netif_up()?;
-    info!("Wifi netif up");
 
     Ok(())
 }
---
+++ rust/3-pid-bm/src/services.rs
@@ -0,0 +1,67 @@
+use core::convert::TryInto;
+
+use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
+
+use esp_idf_hal::modem::Modem;
+use esp_idf_svc::mdns::EspMdns;
+use esp_idf_svc::wifi::{BlockingWifi, EspWifi};
+use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
+use log::info;
+
+pub struct Services<'a> {
+    #[allow(dead_code)] // it holds resources
+    wifi: BlockingWifi<EspWifi<'a>>,
+}
+
+impl<'a> Services<'a> {
+    pub fn new(
+        modem: Modem,
+        ssid: &'static str,
+        password: &'static str,
+    ) -> anyhow::Result<Services<'a>> {
+        let sys_loop = EspSystemEventLoop::take()?;
+        let nvs = EspDefaultNvsPartition::take()?;
+
+        let mut wifi =
+            BlockingWifi::wrap(EspWifi::new(modem, sys_loop.clone(), Some(nvs))?, sys_loop)?;
+
+        let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+        info!("Wifi DHCP info: {:?}", ip_info);
+
+        let mut mdns = EspMdns::take()?;
+        mdns.set_hostname("esp32")?;
+        mdns.add_service(Some("esp32"), "_modbus", "_tcp", 502, &[("board", "esp32")])?;
+
+        Self::connect_wifi(&mut wifi, ssid, password)?;
+
+        Ok(Services { wifi })
+    }
+
+    fn connect_wifi(
+        wifi: &mut BlockingWifi<EspWifi<'static>>,
+        ssid: &'static str,
+        password: &'static str,
+    ) -> anyhow::Result<()> {
+        let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
+            ssid: ssid.try_into().unwrap(),
+            bssid: None,
+            auth_method: AuthMethod::WPA2Personal,
+            password: password.try_into().unwrap(),
+            channel: None,
+            ..Default::default()
+        });
+
+        wifi.set_configuration(&wifi_configuration)?;
+
+        wifi.start()?;
+        info!("Wifi started");
+
+        wifi.connect()?;
+        info!("Wifi connected");
+
+        wifi.wait_netif_up()?;
+        info!("Wifi netif up");
+
+        Ok(())
+    }
+}
