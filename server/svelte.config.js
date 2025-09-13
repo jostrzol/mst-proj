a72a863ebe816c24a6d2bc26fc0b68c4469c80f1
Jakub Ostrzołek <kubaostrzolek@gmail.com>
2025-09-13 19:02:30 +0200

    server: remove cors

--- server/svelte.config.js
+++ server/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-node";
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,8 +8,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		csrf: {
+			trustedOrigins: ['*'],
+		},
+	},
 };
 
 export default config;
